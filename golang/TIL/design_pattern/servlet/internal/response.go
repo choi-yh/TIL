32a33b97a1e6e0793bd1444cb7d88ec0d272a6ca
choi-yh <yhchoi0102@gmail.com>
2024-05-02 23:14:03 +0900

    :rotating_light: style(lint): fix staticcheck

--- golang/TIL/design_pattern/servlet/internal/response.go
+++ golang/TIL/design_pattern/servlet/internal/response.go
@@ -7,22 +7,22 @@ import (
 )
 
 func WriteErrorResponse(conn net.Conn, found int, message string) {
-	fmt.Fprintf(conn, "HTTP/1.1 %d %s\r\n", found, http.StatusText(found))
-	fmt.Fprintf(conn, "Server: dante-server\r\n")
+	fmt.Fprint(conn, "HTTP/1.1 %d %s\r\n", found, http.StatusText(found))
+	fmt.Fprint(conn, "Server: dante-server\r\n")
 
-	fmt.Fprintf(conn, "\n")
+	fmt.Fprint(conn, "\n")
 	if len(message) != 0 {
-		fmt.Fprintf(conn, message)
+		fmt.Fprint(conn, message)
 	} else {
-		fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
+		fmt.Fprint(conn, "Content-Type: text/plain\r\n")
 	}
 }
 
 func WriteResponse(conn net.Conn, s string) {
-	fmt.Fprintf(conn, "HTTP/1.1 %d %s\r\n", http.StatusOK, http.StatusText(http.StatusOK))
-	fmt.Fprintf(conn, "Server: dante-server\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(s))
+	fmt.Fprint(conn, "HTTP/1.1 %d %s\r\n", http.StatusOK, http.StatusText(http.StatusOK))
+	fmt.Fprint(conn, "Server: dante-server\r\n")
+	fmt.Fprint(conn, "Content-Length: %d\r\n", len(s))
 
-	fmt.Fprintf(conn, "\n")
-	fmt.Fprintf(conn, s)
+	fmt.Fprint(conn, "\n")
+	fmt.Fprint(conn, s)
 }
