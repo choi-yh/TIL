7522e44b0fba9ec36f69e3ae37c3fed9b34bd0fc
Younghyo Choi <yhchoi0102@gmail.com>
2024-06-18 22:43:41 +0900

    [level 2] Title: 숫자 변환하기, Time: 603.52 ms, Memory: 17.3 MB -BaekjoonHub

--- algorithm/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ algorithm/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,31 +1,23 @@
 def solution(x, y, n):
-    dp = [0] * (y + 1)
+    maxInt = 1000001
+    dp = [maxInt] * (y + 1)
+    dp[x] = 0
     
     for i in range(x, y + 1):
-        if i != x and dp[i] == 0:
-            dp[i] = -1
-            continue
-        
         plus = i + n
         double = i * 2
         triple = i * 3
         
         if plus < y + 1:
-            if dp[plus] == 0:
-                dp[plus] = dp[i] + 1
-            else:
                 dp[plus] = min(dp[plus], dp[i] + 1)
         
         if double < y + 1:
-            if dp[double] == 0:
-                dp[double] = dp[i] + 1
-            else:
                 dp[double] = min(dp[double], dp[i] + 1)
                 
         if triple < y + 1:
-            if dp[triple] == 0:
-                dp[triple] = dp[i] + 1
-            else:
                 dp[triple] = min(dp[triple], dp[i] + 1)
         
+    if dp[y] == maxInt:
+        return -1
+    
     return dp[y]
