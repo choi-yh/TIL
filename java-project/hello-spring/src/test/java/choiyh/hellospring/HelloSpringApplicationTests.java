de2e6b61f746997e07870dba74093f851653ca30
choi-yh <yhchoi0102@gmail.com>
2024-08-05 16:35:01 +0900

    initial commit

---
+++ java-project/hello-spring/src/test/java/choiyh/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package choiyh.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
