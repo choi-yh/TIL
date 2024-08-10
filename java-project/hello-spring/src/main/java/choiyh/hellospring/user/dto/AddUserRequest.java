4d4ab947ff921b8f7c36e3d99054b51d2df8f77c
choi-yh <yhchoi0102@gmail.com>
2024-08-11 08:35:14 +0900

    feat(spring): 회원가입 & 로그인
    book/springboot3-backend

---
+++ java-project/hello-spring/src/main/java/choiyh/hellospring/user/dto/AddUserRequest.java
@@ -0,0 +1,13 @@
+package choiyh.hellospring.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+
+    private String email;
+    private String password;
+
+}
