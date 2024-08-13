7357d6bb8123fd98b252a576e655ff19fc9f1494
choi-yh <yhchoi0102@gmail.com>
2024-08-13 12:06:31 +0900

    feat(spring): token 발급 API
    - tokenProvider 를 통해서 토큰 생성, 검증 작업 진행 (이 때, JwtProperties 사용)
    - token 은 securityContextHolder 에 저장된다. (세션 기반) -> 서버 내렸다 올리면 날아가는 것은 아닌지?
    - accessToken validation 후에 refreshToken 을 DB 에 저장해서 재발급한다.
    
    book/springboot3-backend

--- java-project/hello-spring/src/main/java/choiyh/hellospring/auth/repository/RefreshTokenRepository.java
+++ java-project/hello-spring/src/main/java/choiyh/hellospring/auth/repository/RefreshTokenRepository.java
@@ -1,5 +1,6 @@
-package choiyh.hellospring.auth;
+package choiyh.hellospring.auth.repository;
 
+import choiyh.hellospring.auth.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
