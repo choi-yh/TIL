5c26399417747cca1ed38e75ae95981199bad4b0
Younghyo Choi <yhchoi0102@gmail.com>
2024-09-29 11:07:10 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- algorithm/프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ algorithm/프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,31 +1,35 @@
-import heapq as hq
+import heapq
 
+"""
+1. 현재 시각을 기준으로 했을 때, 가능한 작업을 꺼내고, 그 중에서 소요시간이 적은 작업을 우선적으로 진행한다.
+
+2. 현재 시각보다 작거나 같은 시작 시간이 가능한 작업. -> 이걸 꺼내서 힙에 넣고, 힙이 빌때까지 작업을 돌린다?
+"""
 def solution(jobs):
     answer = 0
+    n = len(jobs)
     
-    completed_job_cnt = 0
-    job_start_time = 0
-    worked_time = -1
-    waitings = []
+    job_cnt = 0 # 수행한 작업 카운트 (반복문을 진행시키기 위함))
+    cur_time = 0 # 현재 시각
+    last_work_time = -1 # 작업 대상을 추출하기 위한 마지막 작업 시간
+    heapq.heapify(jobs) # 요청 시점을 기준으로 작업 정렬
     
-    while completed_job_cnt < len(jobs):
-        # 작업할 수 있는 대상을 힙에 넣기
+    waiting_jobs = [] # 작업 대상 후보군
+    while job_cnt < n:
         for job in jobs:
-            if worked_time < job[0] <= job_start_time:
-                hq.heappush(waitings, [job[1], job[0]])
-        
-        # 작업 대상이 없다면 현재 시간 증가
-        if len(waitings) == 0:
-            job_start_time += 1
+            if last_work_time < job[0] <= cur_time:
+                heapq.heappush(waiting_jobs, [job[1], job[0]])
+                
+        if len(waiting_jobs) == 0:
+            cur_time += 1
             continue
-            
-        # 총 소요시간이 적게 걸리는 작업
-        j = hq.heappop(waitings)
         
-        answer += j[0] + job_start_time - j[1]
-        worked_time = job_start_time
-        job_start_time += j[0]
+        job = heapq.heappop(waiting_jobs)
         
-        completed_job_cnt += 1
+        answer += (cur_time - job[1]) + job[0]
+        
+        job_cnt += 1
+        last_work_time = cur_time
+        cur_time += job[0]
     
-    return answer // len(jobs)
+    return answer // n
