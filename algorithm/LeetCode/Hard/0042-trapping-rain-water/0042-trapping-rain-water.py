474ccd8a1744a8a8a773b5a3f9cffc6a43227bc1
Younghyo Choi <yhchoi0102@gmail.com>
2024-10-20 11:15:23 +0900

    Time: 11 ms (99.57%), Space: 18.4 MB (88.31%) - LeetHub

--- algorithm/LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.py
+++ algorithm/LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -8,14 +8,13 @@ class Solution:
         while left < right:
             if height[left] <= height[right]:
                 left_max = max(height[left], left_max)
-                if height[left] < left_max:
-                    answer += left_max - height[left]
-
+                # if height[left] < left_max:
+                answer += left_max - height[left]
                 left += 1
             else:
                 right_max = max(height[right], right_max)
-                if height[right] < right_max:
-                    answer += right_max - height[right]
+                # if height[right] < right_max:
+                answer += right_max - height[right]
                 right -= 1
 
         return answer
