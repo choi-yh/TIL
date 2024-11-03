47ca70924d55fe5d20b8ef8263b90385c0b0e3d8
Younghyo Choi <yhchoi0102@gmail.com>
2024-11-03 10:13:33 +0900

    [level 3] Title: 최고의 집합, Time: 1.46 ms, Memory: 11.2 MB -BaekjoonHub

--- algorithm/프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ algorithm/프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,10 +1,11 @@
 def solution(n, s):
+    # n 이 더 큰 경우는 불가능 (1 * n > s)
     if n > s:
         return [-1]
     
     answer = [0] * n
     for i in range(n):
-        answer[i] = s // (n - i)
+        answer[i] = s // (n - i)  # 남은 갯수로 나누기
         s -= answer[i]
     
     return answer
