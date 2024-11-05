fb34a9a289fa3ab6344783a2da911e174b2679d8
Younghyo Choi <yhchoi0102@gmail.com>
2024-11-05 18:53:52 +0900

    Time: 87 ms (16.25%), Space: 16.9 MB (82.29%) - LeetHub

---
+++ algorithm/LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        hashmap = dict()
+
+        for i in range(len(s)):
+            hashmap[s[i]] = hashmap.get(s[i], 0) + 1
+
+        for i in range(len(s)):
+            if hashmap[s[i]] == 1:
+                return i
+
+        return -1
